d632b40229cbad1ca72ce3954d85d4cbe62b70dd
svnix <svnix@protonmail.com>
2024-03-22 17:20:41 +0530

    Updates to autostart and wallpaper_changer scripts

--- .local/bin/wallpaper_changer.sh
+++ .local/bin/wallpaper_changer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DISPLAY=:0 feh --no-fehbg --bg-scale --randomize /usr/share/backgrounds/linuxmint-virginia/*.jpg
+DISPLAY=:0 feh --no-fehbg --bg-scale --randomize --recursive /usr/share/backgrounds/
 
 # Note: Cron job (or systemd service/timer) does NOT work without DISPLAY=:0
 
-# A simple crontab entry would look be as follows to run the script every 10m.
+# A sample crontab entry: runs the script every 10m.
 # m h dom mon dow command
 */10 * * * * /bin/sh ~/.local/bin/wallpaper_changer.sh
